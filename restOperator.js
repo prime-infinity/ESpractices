926417ce614a275e5e65bd113c12cf3d065c9098
prime-infinity <soe.a.prime@gmail.com>
2021-12-24 11:27:16 +0100

    rest operator

---
+++ restOperator.js
@@ -0,0 +1,22 @@
+function add(){
+    //console.log([...arguments])
+}
+
+add(2,3,4,2,1)
+
+const stillAdd = () => {
+    //console.log(arguments); //arguments do not work with arrow functions
+}
+//stillAdd(4,2,3,5)
+
+
+//with rest 
+function addTo(...nums){
+    //console.log(nums);
+}
+addTo(3,2,5,2)
+
+const stillAddTo = (...args) => {
+    console.log(args);//for some reason, this is not working
+}
+stillAdd(3,4,5,2)
