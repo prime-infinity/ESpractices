550cbebc292d83f268d9e528c4fcf256e2164603
prime-infinity <soe.a.prime@gmail.com>
2021-12-24 01:32:51 +0100

    done spreading

--- spreadOperator.js
+++ spreadOperator.js
@@ -19,14 +19,45 @@ const newTeam = [...team1,...team2]
 //copying arrays
 const productList1 = ['phone','adapter']
 const newProductList = [...productList1]
-console.log(newProductList);
+//console.log(newProductList);
+
+
+
+//spread operator with objects
+const emp = {
+    name:'john',
+    job:'developer'
+};
+
+const personalDetails = {
+    age:20
+}
+
+const clonedEmployeed = {...emp}
+//console.log(clonedEmployeed);
+
+const employedDetails = {...emp,...personalDetails}
+//console.log(employedDetails);
+
+//suprising enough, this was it with spread operator and objects
 
 
 
 
+//spread operator with functions
+const scoreList = [100,40,200,43]
+//console.log(Math.min(...scoreList));
+
+const addTonum = x => x + 3
+for (let i = 0; i < scoreList.length; i++) {
+    const e = scoreList[i];
+    //console.log(addTonum(e));
+}
 
 
 
+
+//immutability with spread operator
 
 
 
