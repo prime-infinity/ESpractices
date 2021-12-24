360c862314c9cdc90e76cac8c34beddb773b037d
prime-infinity <soe.a.prime@gmail.com>
2021-12-24 01:04:13 +0100

    spread operator,the illusive

---
+++ spreadOperator.js
@@ -0,0 +1,36 @@
+const users =  ["user1","user2"]
+//console.log(users)
+for (let i = 0; i < users.length; i++) {
+    const element = users[i];
+    //console.log(element);
+}
+
+//console.log(...users);
+
+
+//spread operator with arrays
+const team1 = ['user1','user2'];
+const team2 = ['user3'];
+
+const newTeam = [...team1,...team2]
+//console.log(newTeam);
+
+
+//copying arrays
+const productList1 = ['phone','adapter']
+const newProductList = [...productList1]
+console.log(newProductList);
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * 
+ */
